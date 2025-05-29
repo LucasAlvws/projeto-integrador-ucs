596168ed39afa53d03d64269cd79862275ce5f8e
Lucas Alves <sociallucasalves@gmail.com>
2025-05-29 19:24:45 -0300

    feat: login

--- projeto/core/views.py
+++ projeto/core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from projeto.core.forms import SignUpForm
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_staff = True  # libera acesso ao admin
+            user.save()
+            login(request, user)
+            return redirect('/admin/')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from projeto.core import views as my_views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/signup/', my_views.signup_view, name='signup'),
 ]
